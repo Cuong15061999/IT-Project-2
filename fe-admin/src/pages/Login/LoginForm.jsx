4f8c6974440261b7da3a61102b0243fe1624eb98
Tan Tran <tan.tran@hdwebsoft.dev>
2024-02-20 23:18:56 +0700

    Update login with google

--- fe-admin/src/pages/Login/LoginForm.jsx
+++ fe-admin/src/pages/Login/LoginForm.jsx
@@ -4,7 +4,8 @@ import { FaUser, FaLock } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { useGoogleLogin } from '@react-oauth/google';
 import Cookies from 'js-cookie';
-import { Navigate, useNavigate } from 'react-router-dom/dist';
+import { useNavigate } from 'react-router-dom/dist';
+import axios from 'axios';
 // import { GoogleLogin } from '@react-oauth/google';
 // import axios from 'axios';
 
@@ -13,28 +14,29 @@ import { Navigate, useNavigate } from 'react-router-dom/dist';
 export const LoginForm = () => {
   const navigate = useNavigate();
 
-  const handleAfterLoginWithGoogle = (tokenResponse) => {
+  const handleAfterLoginWithGoogle = async (tokenResponse) => {
     if (Object.entries(tokenResponse).length) {
-      Cookies.set('access_token', tokenResponse.access_token, { expires: tokenResponse.expires_in / 86400 });
-      navigate('/', { replace: true });
+      // after login with google success, call api to get user info this account that user logged in from google api 
+      const { data: userInfo } = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+        headers: {
+          Authorization: `Bearer ${tokenResponse.access_token}`,
+        }
+      })
+
+      // TODO: call api and send this data to verify/save/sync this data with BE
+
+      // next save access_token and user_id logged in to cookies
+      // Cookies.set('access_token', tokenResponse.access_token, { expires: tokenResponse.expires_in / 86400 });
+
+      // after set data to cookie => redirect to home
+      // navigate('/', { replace: true });
       return;
     }
-    navigate('/login', { replace: true });
   }
 
   const loginGoogle = useGoogleLogin({
     onSuccess: handleAfterLoginWithGoogle,
-  }
-    // const loginGoogle = () => { axios.get('http://localhost:3001/auth/google')
-    // .then(function (response) {
-    //   // handle success
-    //   console.log(response);
-    // })
-    // .catch(function (error) {
-    //   // handle error
-    //   console.log(error);
-    // })}
-  )
+  })
 
   return (
     <div className='pagesLogin'>
