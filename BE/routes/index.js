da708812d38d2647626057b2c311e12e246483fe
tuantran150399 <105727082+tuantran150399@users.noreply.github.com>
2024-02-27 00:29:42 +0700

    import file excel
    import file excel

--- BE/routes/index.js
+++ BE/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const xlsx = require('xlsx');
+
+const upload = multer({ dest: 'uploads/' });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', upload.single('excelFile'), (req, res) => {
+  const file = req.file;
+  if (!file) {
+      return res.status(400).send('No file uploaded.');
+  }
+
+  const workbook = xlsx.readFile(file.path);
+  const sheet_name_list = workbook.SheetNames;
+  const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+
+  // Now 'data' contains the JSON representation of your Excel file
+
+  res.send(data);
 });
 
 module.exports = router;
